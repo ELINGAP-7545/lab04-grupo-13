f25b50e7f5cf3525dcb34e9f3bc86ffd21ec5544
roldandiego <62714712+roldandiego@users.noreply.github.com>
2020-04-06 21:13:22 -0500

    7 segmentos

--- 7 segmentos/BCDtoSSeg.v
+++ 7 segmentos/BCDtoSSeg.v
@@ -1,33 +1,40 @@
-module BCDtoSSeg (BCD, SSeg, an);
+module BCDtoSSeg (BCD, SSeg, an); // inicio del modulo
 
-  input [3:0] BCD;
-  output reg [0:6] SSeg;
-  output [3:0] an;
+  input [3:0] BCD; //Entrada 
+  output reg [0:6] SSeg; //salida
+  output [3:0] an; // salida
 
-assign an=4'b1110;
+assign an=4'b1110; // esto quiere decir que estamos inicializando por anodo y un display.
+// assign una asignacion 
 
+//En sistemas de computación,Binary-Coded Decimal (BCD) o Decimal codificado en binario 
+//es un estándar para representar números decimales en el sistema binario, en donde cada//
+// dígito decimal es codificado con una secuencia de 4 bits.
 
-always @ ( * ) begin
-  case (BCD)
-   4'b0000: SSeg = 7'b0000001; // "0"  
-	4'b0001: SSeg = 7'b1001111; // "1" 
-	4'b0010: SSeg = 7'b0010010; // "2" 
-	4'b0011: SSeg = 7'b0000110; // "3" 
-	4'b0100: SSeg = 7'b1001100; // "4" 
-	4'b0101: SSeg = 7'b0100100; // "5" 
-	4'b0110: SSeg = 7'b0100000; // "6" 
-	4'b0111: SSeg = 7'b0001111; // "7" 
-	4'b1000: SSeg = 7'b0000000; // "8"  
-	4'b1001: SSeg = 7'b0000100; // "9" 
+always @ ( * ) begin // Este tipo de procesos se ejecuta continuamente a modo de bucle,
+// y como su propio nombre indica, está continuamente ejecutándose
+
+  case (BCD) // case evalúa una expresión y en función de su valor ejecutará la sentencia o 
+  //grupo de sentencias agrupadas en el primer caso que coincida
+	 	 
+   4'b0000: SSeg = 7'b0000001; // "0"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0001: SSeg = 7'b1001111; // "1"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0010: SSeg = 7'b0010010; // "2"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0011: SSeg = 7'b0000110; // "3"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0100: SSeg = 7'b1001100; // "4"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0101: SSeg = 7'b0100100; // "5"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0110: SSeg = 7'b0100000; // "6"  En este proceso se realiza el cambio de Binario a BCD
+	4'b0111: SSeg = 7'b0001111; // "7"  En este proceso se realiza el cambio de Binario a BCD
+	4'b1000: SSeg = 7'b0000000; // "8"  En este proceso se realiza el cambio de Binario a BCD
+	4'b1001: SSeg = 7'b0000100; // "9"  En este proceso se realiza el cambio de Binario a BCD
    4'ha: SSeg = 7'b0001000;  
    4'hb: SSeg = 7'b1100000;
    4'hc: SSeg = 7'b0110001;
    4'hd: SSeg = 7'b1000010;
    4'he: SSeg = 7'b0110000;
    4'hf: SSeg = 7'b0111000;
-    default:
+    default:  // por defecto 
     SSeg = 0;
-  endcase
-end
-
-endmodule
+  endcase // fin del case
+end // fin del always
+endmodule  // fin del modulo
--- 7 segmentos/display/display.v
+++ 7 segmentos/display/display.v
@@ -1,25 +1,28 @@
-module display(num,clk,sseg,an,rst,led);
-    input [15:0] num;
+//Declaracion del modulo de sus entradas y salidas.
+module display(num,clk,sseg,an,rst,led); //inicio del modulo 
+    input [15:0] num; 
     input clk;
     output [0:6] sseg;
     output reg [3:0] an;
 	 input rst;
 	 output led;
     
-
-
-
-reg [3:0]bcd=0;
+reg [3:0]bcd=0;// Declaración de señales y variables que se utilizarán.
 
  
-BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg));
+BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg)); //Aca estamos llamando al otro pyoyecto y hacer una multiplexacion 
+// para que funcionen 
 
-reg [26:0] cfreq=0;
+reg [26:0] cfreq=0; // Declaración de señales y variables que se utilizarán.
 wire enable;
 
-// Divisor de frecuecia
+/* Divisor de frecuecia: el Docente solicita que cada display debe activarse máximo cada 16 ms.
+   por eso se realiza el divisor de frecuencia cada 16ms,La sentencia condicional if-else 
+	controla la ejecución de otras sentencias y/o asignaciones. En caso de haber múltiples sentencias o asignaciones 
+	es necesario hacer uso del bloque begin-end. La sintaxis de esta estructura es la siguiente. Su función es dividir 
+	la frecuencia de clk de entrada, en el tiempo requerido para cada camvio de ánodo*/
 
-assign enable = cfreq[16];
+assign enable = cfreq[16]; 
 assign led =enable;
 always @(posedge clk) begin
   if(rst==1) begin
@@ -29,7 +32,13 @@ always @(posedge clk) begin
 	end
 end
 
-reg [1:0] count =0;
+reg [1:0] count =0;// Declaración de señales y variables que se utilizarán.
+
+/*Aca vamos a tener una sentencia condicional if y else donde si rst entrada es ==1 entonces count es menor 
+o igual a 0  entonces todos los display quedan encendidos si no entonces entra en ELSE donde nos va a encerder 
+el primer display y va a seguir asi mientras el IF no entre, osea que cada else encedera el primero sumara 1
+y encendera el siguiente display  para eso entra el case por que ella tiene el resultado del if-else = count 
+y se ejecutara para los 4 display  */
 always @(posedge enable) begin
 		if(rst==1) begin
 			count<= 0;
@@ -38,12 +47,17 @@ always @(posedge enable) begin
 			count<= count+1;
 			an<=4'b1101; 
 			case (count) 
+			
+			//Evalúa una expresión y en función de su valor ejecutará la sentencia o 
+         //grupo de sentencias agrupadas en el primer caso que coincida
+			
 				2'h0: begin bcd <= num[3:0];   an<=4'b1110; end 
 				2'h1: begin bcd <= num[7:4];   an<=4'b1101; end 
 				2'h2: begin bcd <= num[11:8];  an<=4'b1011; end 
 				2'h3: begin bcd <= num[15:12]; an<=4'b0111; end 
-			endcase
+			endcase // fin del case
 		end
-      end
+      end //fin del else.
 
 endmodule
+// fin del modulo.
